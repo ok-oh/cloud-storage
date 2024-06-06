73508301d3b3426956bb91172a87f7410a6a29c6
211250063 <211250063@smail.nju.edu.cn>
2024-06-06 09:03:03 +0800

    后端权限划分功能完成，前端未修改

--- cloud/app.py
+++ cloud/app.py
@@ -28,9 +28,51 @@ obsClient = ObsClient(
 TEMP_DIR = os.path.join(os.getcwd(), 'tmp')
 os.makedirs(TEMP_DIR, exist_ok=True)
 
+
+
+def add_permission_to_file(filename, username, permission):
+    for user in filePermission_Collection[filename]:
+        if username in user:
+            filePermission_Collection[filename].remove(user)
+
+
+    filePermission_Collection[filename].append({username:permission})
+
+
+def set_permission_of_new_file(filename, exception):
+    for user in username_collection:
+        if user != exception:
+            add_permission_to_file(filename,user,2)
+        else:
+            add_permission_to_file(filename,user,0)
+
+def get_permission_of_the_file(filename, username):
+    print(filename)
+    print(filePermission_Collection)
+    for user in filePermission_Collection[filename]:
+        if username in user:
+            return user[username]
+    return None
+
+def remove_permission_of_the_file(filename, username):
+    add_permission_to_file(filename,username,2)
+
+def get_root_user_of_the_file(filename):
+    user_found = []
+    for user in username_collection:
+        if get_permission_of_the_file(filename,user) == 0:
+            user_found.append(user)
+    return user_found
+
+
+
+
+
 @app.route('/')
 def home():
     return "Hello, Flask with OBS!"
+
+
 
 
 # 文件上传接口
@@ -47,61 +89,38 @@ def upload_file(username):
 
     resp = obsClient.putFile(bucket_name, file.filename , file_path)
 
-    fileName_Collection.append(file.filename)
-    filePermission_Collection[file.filename] = [username]
+
 
     if resp.status < 300:
         os.remove(file_path)
+        if file.filename not in fileName_Collection:
+            fileName_Collection.append(file.filename)
+            filePermission_Collection[file.filename] = []
 
+        set_permission_of_new_file(file.filename, username)
         user_log[username].append('you upload the file: ' + file.filename)
-
+        print(filePermission_Collection)
         return file.filename+' add success'
     else:
         return 'error'
 
 
-
+#0最高权限(可删除），1下载权限
 
 # 文件下载接口
 @app.route('/download/<filename>/<username>', methods=['GET'])
 def download_file(filename,username):
     download_path ='C:/Users/86189/Downloads/' + filename
 
-
-    for user in filePermission_Collection[filename]:
-        user_log[user].append('your file(' + filename +') was downloaded by ' + username)
-
-
-    if filename in fileName_Collection:
-        if username in filePermission_Collection[filename]:
-            resp = obsClient.getObject(bucket_name, filename, download_path)
-            if resp.status < 300:
-                return filename+' download success', resp.status
-            else:
-                return 'error'
-
-    else:
-        return 'error'
-
-
-
-@app.route('/delete/<filename>/<username>', methods=['DELETE'])
-def delete_file(filename,username):
-
-    if username in filePermission_Collection[filename]:
-        resp = obsClient.deleteObject(bucket_name, filename)
-
-        for user in filePermission_Collection[filename]:
-            user_log[user].append('your file(' + filename + ') was deleted by ' + username)
-
-
-        fileName_Collection.remove(filename)
-        del filePermission_Collection[filename]
-
-
-
+    if get_permission_of_the_file(filename, username) < 2:
+        resp = obsClient.getObject(bucket_name, filename, download_path)
         if resp.status < 300:
-            return 'delete success', resp.status
+
+            for user in get_root_user_of_the_file(filename):
+                user_log[user].append('your file(' + filename + ') was downloaded by ' + user)
+
+            user_log[username].append('you download the file(' + filename + ')')
+            return filename + 'download success'
         else:
             return 'error'
     else:
@@ -110,13 +129,46 @@ def delete_file(filename,username):
 
 
 
+@app.route('/delete/<filename>/<username>', methods=['DELETE'])
+def delete_file(filename,username):
+
+    if username in get_root_user_of_the_file(filename):
+        resp = obsClient.deleteObject(bucket_name, filename)
+
+        if resp.status < 300:
+            for user in get_root_user_of_the_file(filename):
+                user_log[user].append('your file(' + filename + ') was deleted by ' + username)
+            fileName_Collection.remove(filename)
+            del filePermission_Collection[filename]
+            return filename + ' delete success'
+        else:
+            return 'error'
+    else:
+        return 'error'
+
+    # for user in filePermission_Collection[filename]:
+    #     if username in user and user[username] == 0:
+    #         resp = obsClient.deleteObject(bucket_name, filename)
+    #         for log_user in filePermission_Collection[filename]:
+    #             if list(log_user.values())[0] == 0:
+    #                 user_log[list(log_user.keys())[0]].append('your file(' + filename + ') was deleted by ' + username)
+    #         fileName_Collection.remove(filename)
+    #         del filePermission_Collection[filename]
+    #
+    #
+    #         if resp.status < 300:
+    #             return 'delete success', resp.status
+    #         else:
+    #             return 'error'
+
+
+
 @app.route('/list/<username>', methods=['GET'])
 def list_file(username):
     file_returned = []
     for filename in fileName_Collection:
-        if username in filePermission_Collection[filename]:
+        if get_permission_of_the_file(filename,username) < 2:
             file_returned.append(filename)
-
     return file_returned
 
 
@@ -127,6 +179,9 @@ def addNewUser(username, password):
     else:
         username_collection.append(username)
         userpassword_collection.append(password)
+
+        for file in fileName_Collection:
+            add_permission_to_file(file,username,2)
 
         user_log[username] = []
         return 'user add success'
@@ -144,30 +199,51 @@ def checkLogin(username, password):
         return 'permission denied'
 
 
-@app.route('/addPermission/<operator>/<targetFile>/<targetUser>',methods=['POST'])
-def addPermission(operator,targetFile,targetUser):
-    if operator in filePermission_Collection[targetFile]:
-        filePermission_Collection[targetFile].append(targetUser)
-
-        user_log[operator].append('you have given the user(' + targetUser + ') permission to manage the file(' + targetFile + ')' )
-        user_log[targetUser].append('you have been given the permission to manage the file(' + targetFile + ') by the user(' + operator + ')')
-
+@app.route('/addPermission/<operator>/<targetFile>/<targetUser>/<permission_level>',methods=['POST'])
+def addPermission(operator,targetFile,targetUser,permission_level):
+    if operator in get_root_user_of_the_file(targetFile):
+        add_permission_to_file(targetFile, targetUser, int(permission_level))
+        user_log[operator].append('you have given the user(' + targetUser + ') permission level_'+ permission_level +' of the file(' + targetFile + ')')
+        user_log[targetUser].append('you have been given the permission level_' + permission_level + ' of the file(' + targetFile + ') by the user(' + operator + ')')
         return 'permission add success'
     else:
         return 'error'
 
+    # for user in filePermission_Collection[targetFile]:
+    #     if operator in user and user[operator] == 0:
+    # #if operator in filePermission_Collection[targetFile]:
+    #
+    #         for target_user in fileName_Collection[targetFile]:
+    #             if targetUser in target_user:
+    #                 filePermission_Collection[targetFile].romove(target_user)
+    #
+    #         filePermission_Collection[targetFile].append({targetUser,permission_level})
+    #         user_log[operator].append('you have given the user(' + targetUser + ') permission level_'+ permission_level +' of the file(' + targetFile + ')' )
+    #         user_log[targetUser].append('you have been given the permission level_'+ permission_level +' of the file(' + targetFile + ') by the user(' + operator + ')')
+    #
+    #         return 'permission add success'
+    #     else:
+    #         return 'error'
+
 @app.route('/deletePermission/<operator>/<targetFile>/<targetUser>',methods=['DELETE'])
 def deletePermission(operator,targetFile,targetUser):
-    if operator in filePermission_Collection[targetFile]:
-        filePermission_Collection[targetFile].remove(targetUser)
-
-        user_log[operator].append('you have cancelled the user(' + targetUser + ') permission to manage the file(' + targetFile + ')')
-        user_log[targetUser].append('you have been cancelled the permission to manage the file(' + targetFile + ') by the user(' + operator + ')')
-
-        return 'permission delete success'
+    if operator in get_root_user_of_the_file(targetFile):
+        remove_permission_of_the_file(targetFile,targetUser)
+        user_log[operator].append('you have cancelled the user(' + targetUser + ') permission of the file(' + targetFile + ')')
+        user_log[targetUser].append('you have been cancelled the permission of the file(' + targetFile + ') by the user(' + operator + ')')
+        return 'permission cancelled success'
     else:
         return 'error'
-
+    # for user in filePermission_Collection[targetFile]:
+    #     if operator in user and user[operator] == 0:
+    #         filePermission_Collection[targetFile].remove(targetUser)
+    #
+    #         user_log[operator].append('you have cancelled the user(' + targetUser + ') permission of the file(' + targetFile + ')')
+    #         user_log[targetUser].append('you have been cancelled the permission of the file(' + targetFile + ') by the user(' + operator + ')')
+    #
+    #         return 'permission delete success'
+    #     else:
+    #         return 'error'
 
 @app.route('/getLog/<username>',methods=['GET'])
 def getLog(username):
